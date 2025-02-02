ab2d0ef945be62fbb73de9c7500b2edfc175e621
ShiinaNeet <72781583+ShiinaNeet@users.noreply.github.com>
2025-02-02 21:24:36 +0800

    Update moment.js

--- src/utils/moment.js
+++ src/utils/moment.js
@@ -1,10 +1,9 @@
-import moment from 'moment';
+import moment from "moment";
 
 function formatDate(date, format) {
-    return moment(date, 'DD MM YYYY hh:mm:ss').format(format);
+  return moment(date).format(format);
 
-    //return moment(date, 'DD-MMM-YYYY HH:mm').format('MMMM D, YYYY - h:mma');
+  //return moment(date, 'DD-MMM-YYYY HH:mm').format('MMMM D, YYYY - h:mma');
 }
-
 
 export default formatDate;
