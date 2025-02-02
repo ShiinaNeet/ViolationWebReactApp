eb2c4656ea0a5405af3a3299b1d6a50a2da937a9
ShiinaNeet <72781583+ShiinaNeet@users.noreply.github.com>
2025-02-02 21:25:46 +0800

    Create Professor_layout.jsx

---
+++ src/Professor_layout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Navigationbar from "./components/Professor/NavigationBar";
+
+const ProfessorLayout = () => (
+  <div className="overflow-x-scroll:hidden">
+    <Navigationbar />
+    <Outlet />
+  </div>
+);
+
+export default ProfessorLayout;
