6cc1d5eca5f72101aaee1a6e33da0ff5f09173ae
ShiinaNeet <72781583+ShiinaNeet@users.noreply.github.com>
2025-02-02 21:24:56 +0800

    Dean routing changes

--- src/routes/Dean/Home.jsx
+++ src/routes/Dean/Home.jsx
@@ -1,20 +1,20 @@
 import ProgramHeadTable from "@components/Dean/ProgramHeadTable";
-import DepartmentTable from "@components/Dean/DepartmentTable";
+// import DepartmentTable from "@components/Dean/DepartmentTable";
 import { Container } from "@mui/material";
 export default function Home() {
   return (
     <>
       <Container
         sx={{
-          display: "flex",
-          flexDirection: "column",
+          // display: "flex",
+          // flexDirection: "column",
           pt: { xs: 10, sm: 15 },
           pb: { xs: 8, sm: 12 },
           height: "100vh",
         }}
       >
         <ProgramHeadTable />
-        <DepartmentTable />
+        {/* <DepartmentTable /> */}
       </Container>
     </>
   );
