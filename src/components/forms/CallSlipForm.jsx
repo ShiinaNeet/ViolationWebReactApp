bfd8c362854b1d1bbc03820c13d70c9a002bce73
ShiinaNeet <72781583+ShiinaNeet@users.noreply.github.com>
2025-02-24 03:07:43 +0800

    Fixed text formatting

--- src/components/forms/CallSlipForm.jsx
+++ src/components/forms/CallSlipForm.jsx
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
-import { TextField, Button, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import {
+  TextField,
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
 import { motion } from "framer-motion";
 import axios from "axios";
 import PropTypes from "prop-types";
@@ -21,7 +28,7 @@ const CallSlipForm = ({ studentDataToPass, alertMessageFunction }) => {
   });
   const [coordinatorUsers, setCoordinatorUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
@@ -30,13 +37,18 @@ const CallSlipForm = ({ studentDataToPass, alertMessageFunction }) => {
     fetchCoordinator();
     setFormData((prev) => ({
       ...prev,
-        name: studentDataToPass.fullname || "",
-        program: studentDataToPass.course || "",
-        college: "Batangas State University",
-        year: studentDataToPass.year_and_department ? studentDataToPass.year_and_department.split(" - ")[0] : "",
-        campus: "Batangas State University - Nasugbu Campus",
-        report_date: new Date().toISOString().slice(0, 10),
-        report_time: new Date().toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" }),
+      name: studentDataToPass.fullname || "",
+      program: studentDataToPass.course || "",
+      college: "Batangas State University",
+      year: studentDataToPass.year_and_department
+        ? studentDataToPass.year_and_department.split(" - ")[0]
+        : "",
+      campus: "Batangas State University - Nasugbu Campus",
+      report_date: new Date().toISOString().slice(0, 10),
+      report_time: new Date().toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
     }));
   }, [studentDataToPass]);
 
@@ -56,8 +68,7 @@ const CallSlipForm = ({ studentDataToPass, alertMessageFunction }) => {
       }
     } catch (error) {
       console.error("There was an error fetching the data!", error);
-    }
-    finally {
+    } finally {
       setIsLoading(false);
     }
   };
@@ -65,26 +76,38 @@ const CallSlipForm = ({ studentDataToPass, alertMessageFunction }) => {
     setIsLoading(true);
     e.preventDefault();
     console.log(formData);
-    if(Object.values(formData).some((value) => value === "")) {
-      alertMessageFunction(true,"Error", "Please fill out all fields.");
+    if (Object.values(formData).some((value) => value === "")) {
+      alertMessageFunction(true, "Error", "Please fill out all fields.");
       return;
     }
-    axios.post("/form/callslip", formData)
-    .then((response) => {
-      if(response.data.status === "success") {
-        alertMessageFunction(true, "Call Slip Form submitted successfully.", "Success");
-      }
-      else {
-        alertMessageFunction(true,"An error occurred while submitting the form.", "Error");
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      alertMessageFunction(true, "An error occurred while submitting the form.", "Error");
-    })
-    .finally(() => {
-      setIsLoading(false);
-    });
+    axios
+      .post("/form/callslip", formData)
+      .then((response) => {
+        if (response.data.status === "success") {
+          alertMessageFunction(
+            true,
+            "Call Slip Form submitted successfully.",
+            "Success"
+          );
+        } else {
+          alertMessageFunction(
+            true,
+            "An error occurred while submitting the form.",
+            "Error"
+          );
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        alertMessageFunction(
+          true,
+          "An error occurred while submitting the form.",
+          "Error"
+        );
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
   return (
     <motion.div
@@ -93,35 +116,48 @@ const CallSlipForm = ({ studentDataToPass, alertMessageFunction }) => {
       transition={{ duration: 0.5 }}
       className="md:w-[500px] w-full mx-auto bg-white"
     >
-      {isLoading ? "Loading..." : 
       <form onSubmit={handleSubmit} className="space-y-4 w-full mt-4">
-        {Object.keys(formData).map((key, index) => (
-          key !== "coordinator_discipline_head" && (
-            <motion.div
-              key={key}
-              initial={{ opacity: 0, x: -20 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.3, delay: index * 0.1 }}
-            >
-              <TextField
-                fullWidth
-                label={key.replace(/_/g, ' ').replace(/\b\w/g, (c) => c.toUpperCase())}
-                name={key}
-                type={key.includes("date") ? "date" : key === "report_time" ? "time" : "text"}
-                value={formData[key]}
-                onChange={handleChange}
-                required
-              />
-            </motion.div>
-          )
-        ))}
+        {Object.keys(formData).map(
+          (key, index) =>
+            key !== "coordinator_discipline_head" && (
+              <motion.div
+                key={key}
+                initial={{ opacity: 0, x: -20 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.3, delay: index * 0.1 }}
+              >
+                <TextField
+                  fullWidth
+                  label={key
+                    .replace(/_/g, " ")
+                    .replace(/\b\w/g, (c) => c.toUpperCase())}
+                  name={key}
+                  type={
+                    key.includes("date")
+                      ? "date"
+                      : key === "report_time"
+                      ? "time"
+                      : "text"
+                  }
+                  value={formData[key]}
+                  onChange={handleChange}
+                  required
+                />
+              </motion.div>
+            )
+        )}
         <motion.div
           initial={{ opacity: 0, x: -20 }}
           animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.3, delay: Object.keys(formData).length * 0.1 }}
+          transition={{
+            duration: 0.3,
+            delay: Object.keys(formData).length * 0.1,
+          }}
         >
           <FormControl fullWidth required>
-            <InputLabel id="demo-simple-select-helper-label">Coordinator Discipline Head</InputLabel>
+            <InputLabel id="demo-simple-select-helper-label">
+              Coordinator Discipline Head
+            </InputLabel>
             <Select
               labelId="demo-simple-select-helper-label"
               label="Coordinator Discipline Head"
@@ -132,24 +168,35 @@ const CallSlipForm = ({ studentDataToPass, alertMessageFunction }) => {
               inputProps={{ "aria-label": "Without label" }}
               required
             >
-              {coordinatorUsers.filter(user => user.type === "OSD_COORDINATOR").map((coordinator) => (
-                <MenuItem key={coordinator.id} value={coordinator.id}>
-                  {coordinator.first_name + " " + coordinator.last_name}
-                </MenuItem>
-              ))}
+              {coordinatorUsers
+                .filter((user) => user.type === "OSD_COORDINATOR")
+                .map((coordinator) => (
+                  <MenuItem key={coordinator.id} value={coordinator.id}>
+                    {coordinator.first_name + " " + coordinator.last_name}
+                  </MenuItem>
+                ))}
             </Select>
           </FormControl>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, y: 10 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.3, delay: (Object.keys(formData).length + 1) * 0.1 }}
+          transition={{
+            duration: 0.3,
+            delay: (Object.keys(formData).length + 1) * 0.1,
+          }}
         >
-          <Button type="submit" variant="outlined" color="error" fullWidth disabled={isLoading}>
+          <Button
+            type="submit"
+            variant="outlined"
+            color="error"
+            fullWidth
+            disabled={isLoading}
+          >
             {isLoading ? "Submitting..." : "Submit"}
           </Button>
         </motion.div>
-      </form>}
+      </form>
     </motion.div>
   );
 };
