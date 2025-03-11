624b1f630e528b040143c85914125736f83aaa25
ShiinaNeet <72781583+ShiinaNeet@users.noreply.github.com>
2025-03-11 20:51:33 +0800

    Removed NotificationList

---
+++ src/utils/StyledToolBar.js
@@ -0,0 +1,16 @@
+import { alpha, styled, Toolbar } from "@mui/material";
+
+export const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  flexShrink: 0,
+  borderRadius: `calc(${theme.shape.borderRadius}px + 8px)`,
+  backdropFilter: "blur(24px)",
+  border: "1px solid",
+  borderColor: (theme.vars || theme).palette.divider,
+  backgroundColor: theme.vars
+    ? `rgba(${theme.vars.palette.background.defaultChannel} / 0.4)`
+    : alpha(theme.palette.background.default, 0.4),
+  boxShadow: `0px 10px 6px rgba(0, 0, 0, 0.1)`,
+}));
